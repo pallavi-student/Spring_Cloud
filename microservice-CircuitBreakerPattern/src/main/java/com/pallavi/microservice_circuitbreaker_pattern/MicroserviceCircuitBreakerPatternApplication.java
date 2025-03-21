07808f3ff312f5ed3a93105db14ace4e563706ea
pallavi15 <pallavigaharwar15@gmail.com>
2025-03-21 23:28:01 +0530

    [Pallavi] Add . Added Circuit Breaker implementation

---
+++ microservice-CircuitBreakerPattern/src/main/java/com/pallavi/microservice_circuitbreaker_pattern/MicroserviceCircuitBreakerPatternApplication.java
@@ -0,0 +1,13 @@
+package com.pallavi.microservice_circuitbreaker_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCircuitBreakerPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCircuitBreakerPatternApplication.class, args);
+	}
+
+}
